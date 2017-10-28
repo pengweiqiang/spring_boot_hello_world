c4ceffe8c41e69c7b9c5a9a65d1087286bfa0d69
pengweiqiang <pengweiqiang@shopin.cn>
2017-10-28 19:13:02 +0800

    [ADD]添加数据库配置

---
+++ src/main/java/com/will/User.java
@@ -0,0 +1,47 @@
+package com.will;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Created by pengweiqiang on 2017/10/28.
+ */
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String name;
+    private Integer age;
+
+
+    public User(){
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
---
+++ src/main/java/com/will/UserController.java
@@ -0,0 +1,11 @@
+package com.will;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by pengweiqiang on 2017/10/28.
+ */
+@RestController
+public class UserController {
+
+}
